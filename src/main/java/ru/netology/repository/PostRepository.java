d59d6329889435b64c1949b2224fc3d22733b9d8
Nina88 <azarovanina@yandex.ru>
2026-04-02 13:46:19 +0300

    feat: initial Spring MVC project with CRUD functionality

---
+++ src/main/java/ru/netology/repository/PostRepository.java
@@ -0,0 +1,33 @@
+package ru.netology.repository;
+
+import ru.netology.model.Post;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicLong;
+
+@org.springframework.stereotype.Repository
+public class PostRepository {
+    private final ConcurrentHashMap<Long, Post> posts = new ConcurrentHashMap<>();
+    private final AtomicLong counter = new AtomicLong(1);
+
+    public List<Post> all() {
+        return new ArrayList<>(posts.values());
+    }
+
+    public Post getById(long id) {
+        return posts.get(id);
+    }
+
+    public Post save(Post post) {
+        if (post.getId() == 0) {
+            post.setId(counter.getAndIncrement());
+        }
+        posts.put(post.getId(), post);
+        return post;
+    }
+
+    public void removeById(long id) {
+        posts.remove(id);
+    }
+}
